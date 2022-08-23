808eaffc8530b089b0938385a2e8e9e18ee089a0
putinhujlo <100492641+putinhujlo@users.noreply.github.com>
2022-08-23 22:16:50 +0300

    first upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+//Swapping images
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/solar-panels-guide.jpeg') {
+    myImage.setAttribute('src','images/solar-panels-guide-2.png');
+  } else {
+    myImage.setAttribute('src','images/solar-panels-guide.jpeg');
+  }
+}
+
+//Personalized greeting
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    const myName = prompt('Please enter your name.');
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+  
+  if (!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem('name');
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+ 
+  myButton.onclick = () => {
+    setUserName();
+  }
+    
